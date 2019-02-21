75fc6b421790a4a7b91f6b8ee5e7acf7e3a14173
Matt Ruston <mruston@underarmour.com>
2019-02-21 09:47:32 -0600

    Initial Commit

---
+++ CarthageTestB/CarthageTestB.h
@@ -0,0 +1,19 @@
+//
+//  CarthageTestB.h
+//  CarthageTestB
+//
+//  Created by Matthew Ruston on 2/21/19.
+//  Copyright © 2019 Under Armour, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageTestB.
+FOUNDATION_EXPORT double CarthageTestBVersionNumber;
+
+//! Project version string for CarthageTestB.
+FOUNDATION_EXPORT const unsigned char CarthageTestBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageTestB/PublicHeader.h>
+
+
