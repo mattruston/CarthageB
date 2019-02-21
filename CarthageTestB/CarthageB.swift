6aecf2b550f642f1324ea2ab6866093b3fe22254
Matt Ruston <mruston@underarmour.com>
2019-02-21 10:10:57 -0600

    Added dependency on CarthageTestA

--- CarthageTestB/CarthageB.swift
+++ CarthageTestB/CarthageB.swift
@@ -7,11 +7,13 @@
 //
 
 import Foundation
+import CarthageTestA
 
 public class CarthageB {
     public static let shared = CarthageB()
     
     public func work() {
+        CarthageA.shared.work()
         print("Carthage B Ran!")
     }
 }
